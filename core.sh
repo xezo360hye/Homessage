4e21ec6b32d0b8622608d5b842c16d473908daf2
xezo360hye <xezo360hye@gmail.com>
2022-09-13 21:59:58 +0300

    Added License (GPLv3)

--- core.sh
+++ core.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# This program is free software: you can redistribute it and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 # $ADMIN will return 1 if not an admin; I used this in if-else later ("if $ADMIN; then" instead of "if [[ $ADMIN ]]; then")
 # This is neccessary because if ADMIN will be empty/unset if statments will trigger
 ADMIN=false
